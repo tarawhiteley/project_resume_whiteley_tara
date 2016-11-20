25fe61fa70635e82d9c124654d35e338e364a311
Tara Whiteley <tarawhiteley@gmail.com>
2016-11-20 17:37:02 -0500

    add jQuery for bar graph

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
-// responsive nav menu - http://responsive-nav.com/
-
 $(document).ready(function () {
+    // responsive nav
     var nav = responsiveNav(".nav-collapse");
+
+    //skill bar
+    jQuery('.skillbar').each(function () {
+        jQuery(this).find('.skillbar-bar').animate({
+            width: jQuery(this).attr('data-percent')
+        },
+        6000);
+    });
 });
